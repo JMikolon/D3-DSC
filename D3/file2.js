b329d22ec977e9fee37e0a1bd70cb73e02e95473
Jan Mikolon <janmikolon@Jans-MacBook-Pro.local>
2019-01-03 15:27:00 +0100

    new files js

---
+++ D3/file2.js
@@ -0,0 +1,103 @@
+function alertOne(){
+    console.log('zweite file')
+   
+}
+
+function run_cluster(){
+    var width = 600,
+    height = 400,
+    margin = 300,
+    padding = 1, 
+    radius = 6;
+
+var n = 100, 
+    m = 10; 
+
+
+var clusters = new Array(m);
+
+const nodes = d3.range(n).map(function() {
+  var i = Math.floor(Math.random() * m),
+      r = radius,
+      d = {
+        cluster: i,
+        radius: r,
+        x: Math.cos(i / m * 2 * Math.PI) * 100 + width / 2 + Math.random(),
+        y: Math.sin(i / m * 2 * Math.PI) * 100 + height / 2 + Math.random()
+      };
+  
+  if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
+  return d;
+});
+  
+
+var force = d3.forceSimulation()
+
+  .force('center', d3.forceCenter(width/2, height/2))
+
+  // cluster by section
+  .force('cluster', cluster().strength(0.9))
+
+  // apply collision with padding
+  .force('collide', d3.forceCollide(d => d.radius + padding).strength(1))
+
+  .on('tick', layoutTick)
+	.nodes(nodes);
+  
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var node = svg.selectAll("circle")
+    .data(nodes)
+	  .enter().append("circle")
+    .style("fill", function(d) { return 'blue'; });
+  
+function layoutTick() {
+  node
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", function(d) { return d.radius; });
+}
+
+function cluster () {
+
+  var nodes,
+    strength = 0.1;
+
+  function force (alpha) {
+    // scale + curve alpha value
+    alpha *= strength * alpha;
+
+    nodes.forEach(function(d) {
+			var cluster = clusters[d.cluster];
+    	if (cluster === d) return;
+      
+      let x = d.x - cluster.x,
+        y = d.y - cluster.y,
+        l = Math.sqrt(x * x + y * y),
+        r = d.radius + cluster.radius;
+
+      if (l != r) {
+        l = (l - r) / l * alpha;
+        d.x -= x *= l;
+        d.y -= y *= l;
+        cluster.x += x;
+        cluster.y += y;
+      }
+    });
+  }
+
+  force.initialize = function (_) {
+    nodes = _;
+  }
+
+  force.strength = _ => {
+    strength = _ == null ? strength : _;
+    return force;
+  };
+
+  return force;
+}
+
+}
